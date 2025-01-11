fbf802287ccd340c84985d474ddd1badff08ff9c
MalzaCar <88378003+MalzaCAr@users.noreply.github.com>
2025-01-11 17:22:14 +0100

    Debug bs

--- commands/fun shit/kysmeme.js
+++ commands/fun shit/kysmeme.js
@@ -11,6 +11,8 @@ module.exports = {
 	async execute(interaction) {
 		const memes = database.collection('memes');
 
+		await interaction.deferReply();
+
 		//return a random meme(document) from the collection
 		let meme;
 		try {
@@ -20,6 +22,19 @@ module.exports = {
 			meme = [{url: "https://cdn.discordapp.com/attachments/815546700072615968/1327431906266058752/image.png?ex=67830acc&is=6781b94c&hm=66e062d9fe35030554fc18c38b74b67149c336665c3657cc58fdf98a2b1e85f2&"}];
 		}
 
-		await interaction.reply(meme[0].url);
+		/* 
+		prints out every meme and spams the channel for debug purposes
+		
+		let library = [];
+		library.push("a"); //i fucking hate javascript with a passion
+		await memes.find({}).stream().on('data', function(doc) {library.push(doc.url)});
+
+		await interaction.editReply(library[0]);
+
+		for (let i = 1; i < library.length; i++) {
+			await interaction.followUp(library[i]);
+		}*/ 
+
+		await interaction.editReply(meme[0].url);
 	},
 };
